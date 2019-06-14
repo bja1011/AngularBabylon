f29572ec7e03e8af844da3d452b07c735a3238a3
adam <kaczmarek@oskarwegner.pl>
2019-06-14 13:35:43 +0200

    AuthService comments

--- src/app/auth/services/auth.service.ts
+++ src/app/auth/services/auth.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { AUTH_TOKEN_NAME } from '../constants/constants';
-import { of } from 'rxjs';
+import { Observable, of } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -10,11 +10,18 @@ export class AuthService {
   constructor() {
   }
 
-  isAuthenticated() {
-    return of(this.isLogged());
+  /**
+   * Check if user is logged and check @todo: if is authenticated on server.
+   */
+  isAuthenticated(): Observable<boolean> {
+    return this.isLogged();
   }
 
-  isLogged() {
-    return !!localStorage.getItem(AUTH_TOKEN_NAME);
+  /**
+   * Check if user us logged in.
+   * Check for token stored in localstorage. Token can be expired so logged in doesn't mean user is authenticated on server.
+   */
+  isLogged(): Observable<boolean> {
+    return of(!!localStorage.getItem(AUTH_TOKEN_NAME));
   }
 }
