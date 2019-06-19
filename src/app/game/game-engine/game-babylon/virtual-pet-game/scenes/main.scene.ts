d6da7426ad94670adbc3004518d7001f14f1ff15
adam <kaczmarek@oskarwegner.pl>
2019-06-19 13:51:30 +0200

    Babylon scene loader test.

--- src/app/game/game-engine/game-babylon/virtual-pet-game/scenes/main.scene.ts
+++ src/app/game/game-engine/game-babylon/virtual-pet-game/scenes/main.scene.ts
@@ -3,10 +3,15 @@ import * as B from 'babylonjs';
 import cannon from 'cannon';
 import { GameService } from '../../../../services/game.service';
 import { takeWhile } from 'rxjs/operators';
+import { Scene } from 'babylonjs';
+import 'babylonjs-loaders';
+import PointLight = BABYLON.PointLight;
 
 export class MainScene extends MyScene {
   private camera: B.FollowCamera;
   private light: B.Light;
+
+  shadowGenerator: B.ShadowGenerator;
 
   private player: B.Mesh;
 
@@ -20,11 +25,13 @@ export class MainScene extends MyScene {
     this.createObjects();
 
     this.setPhysics();
+
+    this.enablePostProcess();
   }
 
   setCamera() {
     this.camera = new B.FollowCamera('camera1', new B.Vector3(0, 5, 10), this);
-    this.camera.heightOffset = 10;
+    this.camera.heightOffset = 5;
     this.camera.rotationOffset = 0;
     this.camera.cameraAcceleration = 0.005;
     this.camera.maxCameraSpeed = 10;
@@ -37,7 +44,13 @@ export class MainScene extends MyScene {
   }
 
   setLigning() {
-    this.light = new B.HemisphericLight('light1', new B.Vector3(0, 1, 0), this);
+
+    const pointLight = new B.PointLight('l', new B.Vector3(3, 3, 0), this);
+    pointLight.shadowEnabled = true;
+    // pointLight.intensity = 10;
+    this.shadowGenerator = new B.ShadowGenerator(1024, pointLight);
+
+    this.light = new B.HemisphericLight('light1', new B.Vector3(0, 5, 0), this);
   }
 
   setInput(player: B.Mesh) {
@@ -62,40 +75,66 @@ export class MainScene extends MyScene {
   }
 
   private createObjects() {
-    this.player = B.MeshBuilder.CreateBox('box', {width: 1, depth: 1, height: 3}, this);
-    this.player.position.y = 1;
 
-    this.player.material = new B.StandardMaterial('player', this);
-    this.player.material['diffuseColor'] = new B.Color3(1, 1, 1);
+    BABYLON.SceneLoader.ImportMesh('', '/', 'assets/3d/jelly-world.glb', this as any, (meshes) => {
+      meshes.forEach(mesh => mesh.receiveShadows = true);
+      console.log(meshes);
 
-    const ground = B.MeshBuilder.CreateGround('ground', {width: 60, height: 60, subdivisions: 2}, this);
+      this.player = B.MeshBuilder.CreateSphere('player', {diameter: 2, segments: 12}, this);
+      this.player.position.y = 1;
 
-    this.setInput(this.player);
-    this.camera.lockedTarget = this.player;
+      this.player.material = new B.StandardMaterial('player', this);
+      this.player.material['diffuseColor'] = new B.Color3(1, 1, 1);
 
-    // Ground
-    ground.material = new B.StandardMaterial('ground', this);
-    ground.material['diffuseTexture'] = new B.Texture('textures/amiga.jpg', this);
-    ground.material['diffuseTexture'].uScale = 3;
-    ground.material['diffuseTexture'].vScale = 3;
+      this.shadowGenerator.getShadowMap().renderList.push(this.player);
 
-    this.onDisposeObservable.add(() => {
+      // const ground = B.MeshBuilder.CreateGround('ground', {width: 60, height: 60, subdivisions: 2}, this);
+      // ground.position.y = 0.2;
+      // ground.receiveShadows = true;
 
-    });
+      this.setInput(this.player);
+      this.camera.lockedTarget = this.player;
 
-    this.gameService.gameEmitter
-      .pipe(
-        takeWhile(() => !this.destroyed)
-      )
-      .subscribe((event) => {
-        switch (event.name) {
-          case 'setFace':
-            break;
-          case 'setBody':
-            this.player.material['diffuseColor'] = new B.Color3(Math.random(), Math.random(), Math.random());
-            break;
-        }
+      // Ground
+      // ground.material = new B.StandardMaterial('ground', this);
+      // ground.material['diffuseTexture'] = new B.Texture('textures/amiga.jpg', this);
+      // ground.material['diffuseTexture'].uScale = 3;
+      // ground.material['diffuseTexture'].vScale = 3;
+
+      this.onDisposeObservable.add(() => {
+        this.destroyed = true;
       });
+
+      this.gameService.gameEmitter
+        .pipe(
+          takeWhile(() => !this.destroyed)
+        )
+        .subscribe((event) => {
+          switch (event.name) {
+            case 'setFace':
+              break;
+            case 'setBody':
+              this.player.material['diffuseColor'] = new B.Color3(Math.random(), Math.random(), Math.random());
+              this.player.scaling.setAll(0.8 + Math.random() * 0.5);
+              break;
+          }
+        });
+    });
   }
 
+  enablePostProcess() {
+    const ssaoRatio = {
+      ssaoRatio: 0.5,
+      combineRatio: 1.0
+    };
+
+    const ssao = new BABYLON.SSAORenderingPipeline('ssao', this as any, ssaoRatio);
+    ssao.fallOff = 0.000001;
+    ssao.area = 1;
+    ssao.radius = 0.0001;
+    ssao.totalStrength = 1.0;
+    ssao.base = 0.5;
+
+    this.postProcessRenderPipelineManager.attachCamerasToRenderPipeline('ssao', this.camera);
+  }
 }
